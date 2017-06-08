2516ead0c1b8a98427208311d95e79478296523e
karkirowle <szerk.animeblog@gmail.com>
2017-06-08 15:54:05 +0100

    New version
    Contains fix for the bug which resulted responding to voltammetry only
    on every second push. Problem was solved by including a "communicator
    header" to avoid the UARTs reading itself.

--- msp430_uart_sender_v0/msp430_uart_sender_v0.ino
+++ msp430_uart_sender_v0/msp430_uart_sender_v0.ino
@@ -5,45 +5,71 @@ const char* example = "cv";
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  memset(commandbuffer, 0, sizeof(commandbuffer)); // zero init if anything remains in the memory accidently
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //Serial.println("v x 15 20 y 30 40");
 
-  if (Serial.available()) {
+  if (Serial.available()) 
+  {
     delay(10);
     char temp = Serial.read();
-    if (temp == '\n' || j == 20) // 20 is max message length
+    if (temp == '\n')
     {
-      // Serial.println(j);
-      if (temp == '\n' && j < 20)
+      // Reads the last element (depends on after logic)
+      commandbuffer[j++] = temp;
+      // Interprets message
+      if (commandbuffer[1] == 'c' && commandbuffer[2] == 'v') // because of m they are from first
       {
-        commandbuffer[j++] = temp;
-        //Serial.println(commandbuffer); // each to newline
-        if (commandbuffer[0] == 'c' && commandbuffer[1] == 'v')
-        {
-          Serial.println("v x 10 20 y 30 40");
-        }
+        Serial.println("v x 10 20 y 30 40");
       }
+      // Clear after interpretation
       memset(commandbuffer, 0, sizeof(commandbuffer));
-      if (j == 20)
-      {
-        j = 0; // Reset
-        commandbuffer[j++] = temp;
-      }
-      else
-      {
-
-        j = 0; // Reset
-      }
-
-
+      j = 0;
+    }
+    else if (j == 20)
+    {
+      // Design decision - if it is more than 20 than it is overloaded, just clear
+      memset(commandbuffer, 0, sizeof(commandbuffer));
+      j = 0;
     }
     else
     {
       commandbuffer[j++] = temp;
     }
   }
-  delay(500);
+  /* //under revision
+    if (temp == '\n' || j == 20) // 20 is max message length
+    {
+    // Serial.println(j);
+    if (temp == '\n' && j < 20)
+    {
+      commandbuffer[j++] = temp;
+      //Serial.println(commandbuffer); // each to newline
+      if (commandbuffer[0] == 'c' && commandbuffer[1] == 'v')
+      {
+        Serial.println("v x 10 20 y 30 40");
+      }
+    }
+    memset(commandbuffer, 0, sizeof(commandbuffer));
+    if (j == 20)
+    {
+      j = 0; // Reset
+      commandbuffer[j++] = temp;
+    }
+    else
+    {
+
+      j = 0; // Reset
+    }
+
+
+    }
+    else
+    {
+    commandbuffer[j++] = temp;
+    }*/
+delay(10); // 500 ori
 }
--- rfduino_receive_v1/rfduino_receive_v1.ino
+++ rfduino_receive_v1/rfduino_receive_v1.ino
@@ -6,26 +6,51 @@ int j = 0;
 
 void setup() {
 
-  Serial.begin(9600,2,3);
+  Serial.begin(9600, 2, 3);
 
   RFduinoBLE.advertisementData = "CouDow"; // shouldnt be more than 10 characters long RFduinoBLE.deviceName = "Timer"; // name of your RFduino. Will appear when other BLE enabled devices search for it
-
+  memset(commandbuffer, 0, sizeof(commandbuffer));
   RFduinoBLE.begin(); // begin
- //Wire.beginOnPins(2, 3); // Starts the I2C interface with SCL on GPIO 2 and SDA on GPIO 3
- //Wire.begin();        // join i2c bus (address optional for master)
- // SoftwareSerial.begin(9600);  // start serial for output
+  //Wire.beginOnPins(2, 3); // Starts the I2C interface with SCL on GPIO 2 and SDA on GPIO 3
+  //Wire.begin();        // join i2c bus (address optional for master)
+  // SoftwareSerial.begin(9600);  // start serial for output
 
 }
 
 void loop() {
 
-// Sending from COM port
-// In theory this is also good if it receives anything on serial port
-   if (Serial.available()) {
-    delay(10); 
+  // Sending from COM port
+  // In theory this is also good if it receives anything on serial port
+  if (Serial.available()) {
+    delay(10);
     char temp = Serial.read();
-    if (temp == '\n' || j == 20) // 20 is max message length
+    if (temp == '\n')
     {
+      // Reads the last element (depends on after logic)
+      // commandbuffer[j++] = temp; // decided not to have unnecesary linebreak
+      // Interprets message
+      if (commandbuffer[0] != 'm') // Not reading itself
+      {
+        RFduinoBLE.send(commandbuffer, (j+1)); // send the content from the MSP430
+      }
+      // Clear after interpretation
+      memset(commandbuffer, 0, sizeof(commandbuffer));
+      j = 0;
+    }
+    else if (j == 20)
+    {
+      // If more than 20, send what is in buffer than reset
+      RFduinoBLE.send(commandbuffer, (j+1));
+      memset(commandbuffer, 0, sizeof(commandbuffer));
+      j = 0;
+    }
+    else
+    {
+      commandbuffer[j++] = temp;
+    }
+    /*
+      if (temp == '\n' || j == 20) // 20 is max message length
+      {
       Serial.println(commandbuffer);
       RFduinoBLE.send(commandbuffer, (j+1));
       memset(commandbuffer, 0, sizeof(commandbuffer));
@@ -38,25 +63,27 @@ void loop() {
       {
         j = 0; // Reset
       }
-      
-      
-    }
-    else
-    {
-      commandbuffer[j++] = temp;
-    }
-  }
 
+
+      }
+      else
+      {
+      commandbuffer[j++] = temp;
+      }
+    */ // under revision
+  }
 }
 
 
 void RFduinoBLE_onReceive(char *data, int len) {
-
+  // This is what I receive on radio
   // display the first recieved byte
+  Serial.print('m');
   for (int i = 0; i < len; i++)
   {
     Serial.print(data[i]);
   }
-  Serial.println();
+  Serial.print('\r');
+  Serial.print('\n');
 }
 
